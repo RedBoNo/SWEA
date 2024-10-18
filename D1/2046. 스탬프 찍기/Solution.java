c5cf5517ee5f81bf9e759b96d1de763e6ed76923
SimDH <jsy670711@gmail.com>
2024-10-18 19:04:40 +0900

    2046번 문제풀이

---
+++ D1/2046. 스탬프 찍기/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+    주어진 숫자만큼 # 을 출력해보세요.
+
+    주어질 숫자는 100,000 이하다.
+*/
+
+public class Solution {
+
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        sc.close();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            System.out.printf("#");
+        }
+    }
+
+    
+}
